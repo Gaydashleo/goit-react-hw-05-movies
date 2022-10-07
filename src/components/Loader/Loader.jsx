24fd3601662ad4fb1e93f25bef9de04b8ef5de73
Leonid Haidash <Gaydashleo@gmail.com>
2022-10-07 21:15:12 +0300

    Loader

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,15 @@
+import { LoaderWrap, Text } from './loader.styled';
+
+
+function Loader() {
+  return (
+    <>
+      <LoaderWrap>
+      <Text>Loading...</Text>
+    </LoaderWrap>
+    </>
+    
+  );
+}
+
+export default Loader;
