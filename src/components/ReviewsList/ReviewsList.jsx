3bc2ca507021ba5bf5f4c24b3055c1bb5358f7ba
Leonid Haidash <Gaydashleo@gmail.com>
2022-10-10 12:06:03 +0300

    reviewslist

---
+++ src/components/ReviewsList/ReviewsList.jsx
@@ -0,0 +1,29 @@
+import propTypes from 'prop-types';
+import { List, Item, Title,Text } from './ReviewsList.styled';
+
+function ReviewsList({ reviews }) {
+  return (
+    <>
+      <List>
+        {reviews.map(({ id, author, text }) => {
+          return (
+            <Item key={id}>
+              <Title>{author}</Title>
+              <Text>{text}</Text>
+            </Item>
+          );
+        })}
+      </List>
+    </>
+   
+  );
+}
+ReviewsList.propTypes = {
+  rewiews: propTypes.arrayOf(
+    propTypes.shape({
+      id: propTypes.string.isRequired,
+      author: propTypes.string.isRequired,
+      text: propTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
